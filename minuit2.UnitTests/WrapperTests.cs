499a250123a3cc9d782af3503de0ba1b0649e22d
Markus Adamek <markus.adamek@bruker.com>
2025-02-28 08:47:08 +0100

    added unit test project and a simple unit test to check exception behavior through wrapper.

---
+++ minuit2.UnitTests/WrapperTests.cs
@@ -0,0 +1,19 @@
+using minuit2.net;
+
+namespace minuit2.UnitTests;
+
+public class WrapperTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Wrapper_ShouldCorrectlyForwardExceptions()
+    {
+        ExceptionThrowingFcn fcn = new ExceptionThrowingFcn();
+        Migrad migrad = new Migrad(fcn, new MnUserParameterState());
+        Assert.Throws<Exception>(() => migrad.Run());
+    }
+}
