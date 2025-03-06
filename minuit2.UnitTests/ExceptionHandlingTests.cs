9ccce02e2adac66742c8d4d85cf20a18892bb888
Adrian <adrian.walser@dynamic-biosensors.com>
2025-03-06 16:10:47 +0100

    refactor: Revive exception handling tests

---
+++ minuit2.UnitTests/ExceptionHandlingTests.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+using minuit2.net;
+
+namespace minuit2.UnitTests;
+
+public class ExceptionHandlingTests
+{
+    private static Func<double, IList<double>, double> ModelFunctionThrowing(Exception exception) =>
+        (_, _) => throw exception;
+    
+    [Test]
+    public void The_minimization_forwards_exceptions_thrown_by_the_model_function()
+    {
+        var cost = new LeastSquares([1], [2], 3, ModelFunctionThrowing(new TestException("Test message")));
+        var minimizer = new Migrad(cost, new UserParameters());
+        minimizer.Invoking(m => m.Run()).Should().ThrowExactly<TestException>().WithMessage("Test message");
+    }
+
+    private class TestException(string message) : Exception(message);
+}
+
+
+
+
