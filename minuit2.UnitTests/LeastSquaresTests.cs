401e8945b82baacabb83fb6170e4f6d5e1ff8b4d
Adrian <adrian.walser@dynamic-biosensors.com>
2025-03-25 10:18:50 +0100

    refactor: Remove check for parameter count in LeastSquares constructor
    If there are too few parameters, there will be an exception when the
    model is called anyways. The check with calling the model with NaN
    parameter values caused issues for models that don't accept NaN inputs.

--- minuit2.UnitTests/LeastSquaresTests.cs
+++ minuit2.UnitTests/LeastSquaresTests.cs
@@ -6,7 +6,6 @@ namespace minuit2.UnitTests;
 public class LeastSquaresTests
 {
     private static List<double> SomeValues(int count) => Enumerable.Range(0, count).Select(i => (double)i).ToList();
-    private static List<string> SomeParameters(int count) => Enumerable.Range(65, count).Select(i => ((char)i).ToString()).ToList();
     
     [TestCase(0,1)]
     [TestCase(1,10)]
@@ -18,19 +17,6 @@ public class LeastSquaresTests
         var yValues = SomeValues(yValueCount);
         
         var construction = void () => _ = new LeastSquares(xValues, yValues, 0, (_, _) => 0, []);
-        construction.Should().Throw<ArgumentException>();
-    }
-
-
-    [TestCase(0)]
-    [TestCase(3)]
-    public void A_least_squares_cost_function_when_constructed_with_fewer_parameter_names_than_the_number_of_model_parameters_throws_an_exception(
-        int parameterCount)
-    {
-        Func<double ,IList<double>, double> cubicPoly = (x, c) => c[0] + c[1] * x + c[2] * x * x + c[3] * x * x * x;
-        var tooFewParameters = SomeParameters(parameterCount);
-        
-        var construction = void () => _ = new LeastSquares(SomeValues(3), SomeValues(3), 0, cubicPoly, tooFewParameters);
         construction.Should().Throw<ArgumentException>();
     }
 }
--- minuit2.net/LeastSquares.cs
+++ minuit2.net/LeastSquares.cs
@@ -31,9 +31,6 @@ public class LeastSquares : ILeastSquares
     {
         if (x.Count != y.Count || x.Count != yError.Count)
             throw new ArgumentException($"{nameof(x)}, {nameof(y)} and {nameof(yError)} must have the same length");
-
-        if (CannotBeMapped(parameters, model))
-            throw new ArgumentException($"{nameof(parameters)} has fewer elements than the number of parameters in {nameof(model)}");
         
         _data = x.Zip(y, yError).Select(t => new DataPoint(t.First, t.Second, t.Third)).ToList();
         _model = model;
@@ -48,19 +45,6 @@ public class LeastSquares : ILeastSquares
     public IList<string> Parameters { get; }
     public int NumberOfData { get; }
     public bool ShouldScaleCovariances { get; }
-    
-    private static bool CannotBeMapped(IList<string> parameters, Func<double, IList<double>, double> model)
-    {
-        try
-        {
-            _ = model(double.NaN, Enumerable.Repeat(double.NaN, parameters.Count).ToList());
-            return false;
-        }
-        catch (Exception)
-        {
-            return true;
-        }
-    }
 
     public double ValueFor(IList<double> parameterValues) => _data
         .Select(datum => (datum.Y - _model(datum.X, parameterValues)) / datum.YError)
