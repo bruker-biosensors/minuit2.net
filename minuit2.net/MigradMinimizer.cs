5277e0f3bdd6a24c5cf5db02f7eb0fad711eb4cf
Adrian <adrian.walser@bruker.com>
2025-05-07 11:34:27 +0200

    refactor: Extract core minimization logic into method

--- minuit2.net/MigradMinimizer.cs
+++ minuit2.net/MigradMinimizer.cs
@@ -12,24 +12,32 @@ public static class MigradMinimizer
         ThrowIfParametersAreNotMatchingBetween(costFunction, parameterConfigurations);
         
         configuration ??= new MigradMinimizerConfiguration();
-        var wrappedCostFunction = new CostFunctionWrap(costFunction);
-        var parameterState = parameterConfigurations.OrderedBy(costFunction.Parameters).AsState();
-        var strategy = new MnStrategy((uint)configuration.Strategy);
-        var migrad = new MnMigradWrap(wrappedCostFunction, parameterState, strategy);
-
-        var minimum = migrad.Run(configuration.MaximumFunctionCalls, configuration.Tolerance);
-        var result = new MinimizationResult(minimum, costFunction);
+        var result = CoreMinimize(costFunction, parameterConfigurations, configuration);
         if (!costFunction.RequiresErrorDefinitionAutoScaling) return result;
         
         costFunction.AutoScaleErrorDefinitionBasedOn(result.ParameterValues.ToList(), result.Variables.ToList());
-        return HesseErrorCalculator.Update(result, costFunction);
+        return HesseErrorCalculator.Update(result, costFunction, configuration.Strategy);
     }
-
+    
     private static void ThrowIfParametersAreNotMatchingBetween(ICostFunction costFunction,
         IReadOnlyCollection<ParameterConfiguration> parameterConfigurations)
     {
         if (parameterConfigurations.AreNotMatching(costFunction.Parameters))
             throw new ArgumentException("The given parameter configurations do not match the parameter names defined " +
                                         $"by the cost function: {costFunction.Parameters}");
+    }
+
+    private static MinimizationResult CoreMinimize(
+        ICostFunction costFunction,
+        IReadOnlyCollection<ParameterConfiguration> parameterConfigurations, 
+        MigradMinimizerConfiguration configuration)
+    {
+        var cost = new CostFunctionWrap(costFunction);
+        var parameterState = parameterConfigurations.OrderedBy(costFunction.Parameters).AsState();
+        var strategy = new MnStrategy((uint)configuration.Strategy);
+        var migrad = new MnMigradWrap(cost, parameterState, strategy);
+
+        var minimum = migrad.Run(configuration.MaximumFunctionCalls, configuration.Tolerance);
+        return new MinimizationResult(minimum, costFunction);
     }
 }
