cb9e851f8f71a1d319b883a95a0e302a6643cad1
Adrian <adrian.walser@bruker.com>
2025-07-02 11:04:33 +0200

    refactor: Extract adding of parameter configuration to parameter state into method

--- minuit2.net/ParameterConfigurations.cs
+++ minuit2.net/ParameterConfigurations.cs
@@ -73,23 +73,25 @@ internal static class ParameterConfigurationExtensions
     public static MnUserParameterState AsState(this IEnumerable<ParameterConfiguration> parameterConfigurations)
     {
         var state = new MnUserParameterState();
-        foreach (var parameter in parameterConfigurations)
-        {
-            state.Add(parameter.Name, parameter.Value, parameter.Value * 0.01);
-            
-            if (parameter.IsFixed) state.Fix(parameter.Name);
-            
-            if(parameter.HasLowerLimit && parameter.HasUpperLimit) 
-                state.SetLimits(parameter.Name, parameter.LowerLimit!.Value, parameter.UpperLimit!.Value);
-            else if (parameter.HasLowerLimit) 
-                state.SetLowerLimit(parameter.Name, parameter.LowerLimit!.Value);
-            else if (parameter.HasUpperLimit) 
-                state.SetUpperLimit(parameter.Name, parameter.UpperLimit!.Value);
-        }
-        
+        foreach (var parameter in parameterConfigurations) state.Add(parameter);
         return state;
     }
-    
+
+    private static void Add(this MnUserParameterState state, ParameterConfiguration parameter)
+    {
+        state.Add(parameter.Name, parameter.Value, parameter.Value * 0.01);
+
+        if (parameter.IsFixed) 
+            state.Fix(parameter.Name);
+
+        if (parameter.HasLowerLimit && parameter.HasUpperLimit)
+            state.SetLimits(parameter.Name, parameter.LowerLimit!.Value, parameter.UpperLimit!.Value);
+        else if (parameter.HasLowerLimit) 
+            state.SetLowerLimit(parameter.Name, parameter.LowerLimit!.Value);
+        else if (parameter.HasUpperLimit) 
+            state.SetUpperLimit(parameter.Name, parameter.UpperLimit!.Value);
+    }
+
     public static bool AreNotMatching(this IReadOnlyCollection<ParameterConfiguration> parameterConfigurations, 
         IList<string> parameterNames)
     {
