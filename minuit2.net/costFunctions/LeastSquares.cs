35b1399c5a66fd64822f747819a112e9e8cae1fe
Adrian Walser <adrian.walser@bruker.com>
2025-08-11 17:18:45 +0200

    refactor: Revise LeastSquares implementation
    Avoid LINQ and additional storage for private DataPoint collection.

--- minuit2.net/costFunctions/LeastSquares.cs
+++ minuit2.net/costFunctions/LeastSquares.cs
@@ -6,7 +6,9 @@ public class LeastSquares : ICostFunction
     // (4 would correspond to 2-sigma errors, 9 would correspond to 3-sigma errors etc.)
     public const double OneSigmaErrorDefinition = 1;
     
-    private readonly List<DataPoint> _data;
+    private readonly IList<double> _x;
+    private readonly IList<double> _y;
+    private readonly IList<double> _yError;
     private readonly Func<double, IList<double>, double> _model;
     private readonly Func<double, IList<double>, IList<double>>? _modelGradient;
     
@@ -21,7 +23,9 @@ public class LeastSquares : ICostFunction
         if (x.Count != y.Count || x.Count != yError.Count)
             throw new ArgumentException($"{nameof(x)}, {nameof(y)} and {nameof(yError)} must have the same length");
 
-        _data = x.Zip(y, yError).Select(t => new DataPoint(t.First, t.Second, t.Third)).ToList();
+        _x = x;
+        _y = y;
+        _yError = yError;
         _model = model;
         _modelGradient = modelGradient;
         
@@ -34,23 +38,32 @@ public class LeastSquares : ICostFunction
     public bool HasGradient { get; }
     public double ErrorDefinition { get; }
     
-    public double ValueFor(IList<double> parameterValues) => 
-        _data.Select(datum => ResidualFor(datum, parameterValues)).Select(residual => residual * residual).Sum();
+    public double ValueFor(IList<double> parameterValues)
+    {
+        double sum = 0;
+        for (var i = 0; i < _x.Count; i++)
+        {
+            var residual = ResidualFor(i, parameterValues);
+            sum += residual * residual;
+        }
+        
+        return sum;
+    }
 
     public IList<double> GradientFor(IList<double> parameterValues)
     {
-        var gradientSums = Enumerable.Repeat(0d, Parameters.Count).ToList();
-        foreach (var datum in _data)
+        var gradientSums = new double[Parameters.Count];
+        for (var i = 0; i < _x.Count; i++)
         {
-            var gradients = _modelGradient!(datum.X, parameterValues);
-            var factor = 2 * ResidualFor(datum, parameterValues);
-            for (var i = 0; i < Parameters.Count; i++) 
-                gradientSums[i] -= factor * gradients[i] / datum.YError;          
+            var factor = 2 * ResidualFor(i, parameterValues);
+            var gradients = _modelGradient!(_x[i], parameterValues);
+            for (var j = 0; j < Parameters.Count; j++) 
+                gradientSums[j] -= factor * gradients[j] / _yError[i];
         }
-
+        
         return gradientSums;
     }
 
-    private double ResidualFor(DataPoint datum, IList<double> parameterValues) =>
-        (datum.Y - _model(datum.X, parameterValues)) / datum.YError;
+    private double ResidualFor(int i, IList<double> parameterValues) =>
+        (_y[i] - _model(_x[i], parameterValues)) / _yError[i];
 }
