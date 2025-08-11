f545e77cbe7654a05ea4cb817847478aa1bbfe5a
Adrian Walser <adrian.walser@bruker.com>
2025-08-11 18:21:12 +0200

    refactor: Slightly extend comment

--- minuit2.net/costFunctions/ComponentCostFunction.cs
+++ minuit2.net/costFunctions/ComponentCostFunction.cs
@@ -3,8 +3,11 @@ namespace minuit2.net.costFunctions;
 internal class ComponentCostFunction(ICostFunction inner, IList<string> compositeParameters) : ICostFunction
 {
     // To achieve proper scaling of component gradients (both analytical and numerically approximated), the function
-    // values and gradients have to be scaled by 1/ErrorDefinition in place. Yet, doing so necessitates re-scaling of
-    // the final function values (after minimization). This must be done in the hosting composite class.
+    // values and gradients have to be scaled by 1 / ErrorDefinition within the local ValueFor and GradientFor methods.
+    // This is because different components may have different ErrorDefinitions, so there is no global scaling that
+    // could be applied by the hosting composite cost function.
+    // Yet doing so requires re-scaling of the final function values (after minimization). This must be done by the
+    // hosting composite cost function.
     
     private readonly List<int> _parameterIndices = inner.Parameters.Select(compositeParameters.IndexOf).ToList();
     
