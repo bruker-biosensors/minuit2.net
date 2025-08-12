cd3120e2feaf37ab07b81b2b4a80e715ba48579b
Adrian Walser <adrian.walser@bruker.com>
2025-08-12 12:18:27 +0200

    refactor: Reformat code to fit line

--- minuit2.net/CostFunctions/ComponentCostFunctionRequiringErrorDefinitionAdjustment.cs
+++ minuit2.net/CostFunctions/ComponentCostFunctionRequiringErrorDefinitionAdjustment.cs
@@ -7,6 +7,12 @@ internal class ComponentCostFunctionRequiringErrorDefinitionAdjustment(
 {
     private string[] Belonging(IList<string> variables) => variables.Where(var => Parameters.Contains(var)).ToArray();
 
-    public ICostFunctionRequiringErrorDefinitionAdjustment WithErrorDefinitionAdjustedBasedOn(IList<double> parameterValues, IList<string> variables) =>
-        new ComponentCostFunctionRequiringErrorDefinitionAdjustment(inner.WithErrorDefinitionAdjustedBasedOn(Belonging(parameterValues), Belonging(variables)), CompositeParameters);
+    public ICostFunctionRequiringErrorDefinitionAdjustment WithErrorDefinitionAdjustedBasedOn(
+        IList<double> parameterValues, 
+        IList<string> variables)
+    {
+        return new ComponentCostFunctionRequiringErrorDefinitionAdjustment(
+            inner.WithErrorDefinitionAdjustedBasedOn(Belonging(parameterValues), Belonging(variables)),
+            CompositeParameters);
+    }
 }
