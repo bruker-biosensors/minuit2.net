32abbeea17cb1eea23633b54febb119aa47a4989
Adrian Walser <adrian.walser@bruker.com>
2025-08-12 12:53:54 +0200

    refactor: Revise comment

--- minuit2.net/CostFunctions/LeastSquaresWithUnknownYError.cs
+++ minuit2.net/CostFunctions/LeastSquaresWithUnknownYError.cs
@@ -20,13 +20,16 @@ internal class LeastSquaresWithUnknownYError : LeastSquaresWithUniformYError, IC
     
     public ICostFunctionRequiringErrorDefinitionAdjustment WithErrorDefinitionAdjustedBasedOn(IList<double> parameterValues, IList<string> variables)
     {
-        // Auto-scale the error definition such that a re-evaluation -- e.g. by a subsequent minimization or accurate
+        // Auto-adjust the error definition such that a re-evaluation -- e.g., by a subsequent minimization or accurate
         // covariance computation (Hesse algorithm) -- yields the same parameter covariances that would be obtained
-        // from a minimization using data y-errors resulting in a reduced chi-squared value of 1.
-        // Yet, in contrast to scaling the y-errors, by scaling the error definition the cost value (chi-squared value)
-        // itself won't be affected. This means that for missing y-errors, the reduced chi-squared should approximate
-        // the variance of the noise overlying the data.
-        // This is equivalent to the default behaviour in lmfit:
+        // from a minimization using an error definition of 1, and "perfect y-errors" resulting in a reduced chi-squared
+        // value of 1.
+        
+        // Yet in contrast to adjusting/estimating the y-errors, by adjusting the error definition, the cost value
+        // (chi-squared value) itself won't be affected. This means that the resulting reduced chi-squared should
+        // approximate the variance of the noise overlying the data.
+        
+        // This is equivalent to the default behavior in lmfit:
         // https://lmfit.github.io/lmfit-py/fitting.html#uncertainties-in-variable-parameters-and-their-correlations
         
         var degreesOfFreedom = X.Count - variables.Count;
