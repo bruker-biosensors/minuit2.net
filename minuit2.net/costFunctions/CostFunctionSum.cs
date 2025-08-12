3c33f162d2e4d716ac1accf0ee5e3e9ab50be1c8
Adrian Walser <adrian.walser@bruker.com>
2025-08-12 10:17:15 +0200

    refactor: Merge ICostFunction into ICompositeCostFunction again

--- minuit2.net/costFunctions/CostFunctionSum.cs
+++ minuit2.net/costFunctions/CostFunctionSum.cs
@@ -1,6 +1,6 @@
 namespace minuit2.net.costFunctions;
 
-internal class CostFunctionSum : ICostFunction, ICompositeCostFunction
+internal class CostFunctionSum : ICompositeCostFunction
 {
     protected readonly ICostFunction[] Components;
 
--- minuit2.net/costFunctions/ICompositeCostFunction.cs
+++ minuit2.net/costFunctions/ICompositeCostFunction.cs
@@ -1,6 +1,6 @@
 namespace minuit2.net.costFunctions;
 
-public interface ICompositeCostFunction
+public interface ICompositeCostFunction : ICostFunction
 {
     double CompositeValueFor(IList<double> parameterValues);
 }
