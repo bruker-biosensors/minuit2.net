1bd775a4aa7b627eeffea869966c8c790dcfdeb9
Adrian Walser <adrian.walser@bruker.com>
2025-09-04 11:59:36 +0200

    refactor: Reduce code duplication in ParameterConfigurationExtensions test utilities

--- minuit2.UnitTests/TestUtilities/ParameterConfigurationExtensions.cs
+++ minuit2.UnitTests/TestUtilities/ParameterConfigurationExtensions.cs
@@ -4,26 +4,17 @@ namespace minuit2.UnitTests.TestUtilities;
 
 internal static class ParameterConfigurationExtensions
 {
-    public static ParameterConfiguration WithSuffix(this ParameterConfiguration parameter, string suffix)
-    {
-        var name = $"{parameter.Name}_{suffix}";
-        return parameter.IsFixed
-            ? ParameterConfiguration.Fixed(name, parameter.Value)
-            : ParameterConfiguration.Variable(name, parameter.Value, parameter.LowerLimit, parameter.UpperLimit);
-    }
+    public static ParameterConfiguration Fixed(this ParameterConfiguration parameter) =>
+        parameter.CopyWith(isFixed: true);
+
+    public static ParameterConfiguration WithSuffix(this ParameterConfiguration parameter, string suffix) =>
+        parameter.CopyWith(name: $"{parameter.Name}_{suffix}");
 
     public static ParameterConfiguration WithValue(this ParameterConfiguration parameter, double value) =>
-        parameter.IsFixed
-            ? ParameterConfiguration.Fixed(parameter.Name, value)
-            : ParameterConfiguration.Variable(parameter.Name, value, parameter.LowerLimit, parameter.UpperLimit);
+        parameter.CopyWith(value: value);
     
     public static ParameterConfiguration WithLimits(this ParameterConfiguration parameter, double? lowerLimit, double? upperLimit) =>
-        parameter.IsFixed
-            ? ParameterConfiguration.Fixed(parameter.Name, parameter.Value)
-            : ParameterConfiguration.Variable(parameter.Name, parameter.Value, lowerLimit, upperLimit);
-
-    public static ParameterConfiguration Fixed(this ParameterConfiguration parameter) =>
-        ParameterConfiguration.Fixed(parameter.Name, parameter.Value);
+        parameter.CopyWith(lowerLimit: lowerLimit, upperLimit: upperLimit);
     
     public static ParameterConfiguration[] WithLimits(
         this IEnumerable<ParameterConfiguration> parameters, 
@@ -31,5 +22,31 @@ internal static class ParameterConfigurationExtensions
         double? upperLimit)
     {
         return parameters.Select(p => p.WithLimits(lowerLimit, upperLimit)).ToArray();
+    }
+
+    private static ParameterConfiguration CopyWith(
+        this ParameterConfiguration parameter,
+        Override<bool>? isFixed = null,
+        Override<string>? name = null,
+        Override<double>? value = null,
+        Override<double?>? lowerLimit = null,
+        Override<double?>? upperLimit = null)
+    {
+        var newIsFixed = Resolve(isFixed, parameter.IsFixed);
+        var newName = Resolve(name, parameter.Name);
+        var newValue = Resolve(value, parameter.Value);
+        var newLowerLimit = Resolve(lowerLimit, parameter.LowerLimit);
+        var newUpperLimit = Resolve(upperLimit, parameter.UpperLimit);
+        
+        return newIsFixed
+            ? ParameterConfiguration.Fixed(newName, newValue)
+            : ParameterConfiguration.Variable(newName, newValue, newLowerLimit, newUpperLimit);
+
+        T Resolve<T>(Override<T>? @override, T old) => @override is not null ? @override.Value : old;
+    }
+    
+    private record Override<T>(T Value)
+    {
+        public static implicit operator Override<T>(T value) => new(value);
     }
 }
