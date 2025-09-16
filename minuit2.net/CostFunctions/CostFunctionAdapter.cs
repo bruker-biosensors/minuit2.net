868e1c001c2176c3d49e1fa33e8ff31609477ba0
Adrian Walser <adrian.walser@bruker.com>
2025-09-16 17:01:58 +0200

    refactor: Shorten comment

--- minuit2.net/CostFunctions/CostFunctionAdapter.cs
+++ minuit2.net/CostFunctions/CostFunctionAdapter.cs
@@ -7,7 +7,6 @@ internal sealed class CostFunctionAdapter(ICostFunction function, CancellationTo
     // the inner ValueFor() and GradientFor() by the error definition directly. This allows us to dynamically adjust
     // error definitions, e.g. to get meaningful parameter uncertainties for least squares cost functions with unknown
     // data errors (see LeastSquaresWithUnknownYError.cs).
-    // Mind that this means that the final cost function value must be rescaled by the error definition!
     public override double Up() => 1;
 
     public override double Cost(VectorDouble parameterValues)
