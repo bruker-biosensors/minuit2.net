5f6be924442df9aa98ed43b9e070b82965e28135
Adrian Walser <adrian.walser@bruker.com>
2025-10-09 17:35:58 +0200

    refactor: Remove unused test assertion extension

--- minuit2.UnitTests/TestUtilities/GenericCollectionAssertionExtensions.cs
+++ minuit2.UnitTests/TestUtilities/GenericCollectionAssertionExtensions.cs
@@ -8,13 +8,6 @@ namespace minuit2.UnitTests.TestUtilities;
 [SuppressMessage("ReSharper", "UnusedMethodReturnValue.Global", Justification = "Adhere to convention")]
 internal static class GenericCollectionAssertionExtensions
 {
-    public static AndConstraint<GenericCollectionAssertions<T>> Fulfill<T>(
-        this GenericCollectionAssertions<T> parent,
-        Action<IEnumerable<T>> assertion)
-    {
-        return parent.Satisfy(assertion);
-    }
-
     public static AndConstraint<GenericCollectionAssertions<double>> BeApproximately(
         this GenericCollectionAssertions<double> parent,
         IEnumerable<double> expectation,
