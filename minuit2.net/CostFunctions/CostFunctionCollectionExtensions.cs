63f6840ce39142a67f22b9f8d2ec52b324b52902
Adrian Walser <adrian.walser@bruker.com>
2025-11-03 17:59:14 +0100

    refactor: Revise/simplify ComponentCostFunction
    - All "error definition scaling magic" is now performed in the
    CostFunctionSum.
    - Components return the inner
    .WithErrorDefinitionRecalculatedBasedOnValid to avoid
    unnecessary nesting complexity

--- minuit2.net/CostFunctions/CostFunctionCollectionExtensions.cs
+++ minuit2.net/CostFunctions/CostFunctionCollectionExtensions.cs
@@ -2,8 +2,8 @@ namespace minuit2.net.CostFunctions;
 
 internal static class CostFunctionCollectionExtensions
 {
-    public static string[] DistinctParameters(this IEnumerable<ICostFunction> costFunctions) =>
-        costFunctions.Aggregate(Enumerable.Empty<string>(), Union).ToArray();
+    public static List<string> DistinctParameters(this IEnumerable<ICostFunction> costFunctions) =>
+        costFunctions.Aggregate(Enumerable.Empty<string>(), Union).ToList();
 
     private static IEnumerable<string> Union(IEnumerable<string> parameters, ICostFunction cost) =>
         parameters.Union(cost.Parameters);
--- minuit2.net/CostFunctions/CostFunctionSum.cs
+++ minuit2.net/CostFunctions/CostFunctionSum.cs
@@ -2,51 +2,41 @@ namespace minuit2.net.CostFunctions;
 
 internal class CostFunctionSum : ICompositeCostFunction
 {
-    private readonly ICostFunction[] _components;
-    private readonly string[] _compositeParameters;
+    private readonly ComponentCostFunction[] _components;
 
     public CostFunctionSum(params ICostFunction[] components)
     {
-        _compositeParameters = components.DistinctParameters();
-        Parameters = _compositeParameters;
+        var parameters = components.DistinctParameters();
+        Parameters = parameters;
         HasGradient = components.All(c => c.HasGradient);
-        ErrorDefinition = 1;  // Neutral element; Scaling is performed within the components since factors may differ.
+        ErrorDefinition = 1;  // Neutral element; Scaling is performed for each component individually since factors may differ.
         
-        _components = components.Select(AsComponentCostFunction).ToArray();
-    }
-
-    private ICostFunction AsComponentCostFunction(ICostFunction costFunction)
-    {
-        return costFunction is ComponentCostFunction
-            ? costFunction
-            : new ComponentCostFunction(costFunction, _compositeParameters);
+        _components = components.Select(c => new ComponentCostFunction(c, parameters)).ToArray();
     }
 
     public IReadOnlyList<string> Parameters { get; }
     public bool HasGradient { get; }
     public double ErrorDefinition { get; }
 
-    public double ValueFor(IReadOnlyList<double> parameterValues) =>
-        _components.Select(c => c.ValueFor(parameterValues)).Sum();
+    public double ValueFor(IReadOnlyList<double> parameterValues) => 
+        _components.Sum(c => c.ValueFor(parameterValues) / c.ErrorDefinition);
 
     public IReadOnlyList<double> GradientFor(IReadOnlyList<double> parameterValues)
     {
         var gradients = new double[Parameters.Count];
-        foreach (var componentGradients in _components.Select(c => c.GradientFor(parameterValues))) 
-            Add(componentGradients, gradients);
-
+        foreach (var component in _components)
+        {
+            var componentGradients = component.GradientFor(parameterValues);
+            for (var i = 0; i < Parameters.Count; i++)
+                gradients[i] += componentGradients[i] / component.ErrorDefinition;
+        }
+        
         return gradients;
     }
-    
-    private void Add(IReadOnlyList<double> componentGradients, double[] gradients)
-    {
-        for (var i = 0; i < Parameters.Count; i++) 
-            gradients[i] += componentGradients[i];
-    }
+
+    public double CompositeValueFor(IReadOnlyList<double> parameterValues) =>
+        _components.Sum(c => c.ValueFor(parameterValues));
 
     public ICostFunction WithErrorDefinitionRecalculatedBasedOnValid(IMinimizationResult result) =>
         new CostFunctionSum(_components.Select(c => c.WithErrorDefinitionRecalculatedBasedOnValid(result)).ToArray());
-    
-    public double CompositeValueFor(IReadOnlyList<double> parameterValues) =>
-        _components.Select(c => c.ValueFor(parameterValues) * c.ErrorDefinition).Sum();
 }
