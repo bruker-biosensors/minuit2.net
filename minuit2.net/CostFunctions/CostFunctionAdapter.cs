82bc5ab04b368bd4216cc805ade4261a23d660ef
Adrian Walser <adrian.walser@bruker.com>
2025-11-13 12:05:33 +0100

    refactor: Auto-reformat code

--- minuit2.net/CostFunctions/CostFunctionAdapter.cs
+++ minuit2.net/CostFunctions/CostFunctionAdapter.cs
@@ -9,7 +9,7 @@ namespace minuit2.net.CostFunctions;
 internal sealed class CostFunctionAdapter(ICostFunction function, CancellationToken cancellationToken) : FCNWrap
 {
     private int _numberOfFunctionCalls;
-    
+
     public ConcurrentQueue<Exception> Exceptions { get; } = new();
 
     // We always forward a neutral error definition (Up) of 1 to the C++ code. Instead, we scale the output values of
@@ -17,13 +17,13 @@ internal sealed class CostFunctionAdapter(ICostFunction function, CancellationTo
     // error definitions, e.g. to get meaningful parameter uncertainties for least squares cost functions with unknown
     // data errors (see LeastSquaresWithUnknownYError.cs).
     public override double Up() => 1;
-    
+
     public override bool HasGradient() => function.HasGradient;
 
     public override double CalculateValue(VectorDouble parameterValues)
     {
         Interlocked.Increment(ref _numberOfFunctionCalls);
-        
+
         try
         {
             return ValueFor(parameterValues);
@@ -47,7 +47,7 @@ internal sealed class CostFunctionAdapter(ICostFunction function, CancellationTo
             return VectorDouble.Repeat(double.NaN, parameterValues.Count);
         }
     }
-    
+
     private double ValueFor(VectorDouble parameterValues)
     {
         if (cancellationToken.IsCancellationRequested)
@@ -55,15 +55,15 @@ internal sealed class CostFunctionAdapter(ICostFunction function, CancellationTo
 
         var value = function.ValueFor(parameterValues.AsReadOnly()) / function.ErrorDefinition;
         return double.IsFinite(value)
-            ? value 
+            ? value
             : throw new MinimizationAbort(NonFiniteValue, parameterValues, _numberOfFunctionCalls);
     }
 
     private VectorDouble GradientFor(VectorDouble parameterValues)
     {
         var gradient = new VectorDouble(function.GradientFor(parameterValues.AsReadOnly()).Select(g => g / function.ErrorDefinition));
-        return gradient.All(double.IsFinite) 
-            ? gradient 
+        return gradient.All(double.IsFinite)
+            ? gradient
             : throw new MinimizationAbort(NonFiniteGradient, parameterValues, _numberOfFunctionCalls);
     }
 
