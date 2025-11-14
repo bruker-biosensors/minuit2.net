cf4aa543724c09676177aeb8d090406c67363b75
Adrian Walser <adrian.walser@bruker.com>
2025-11-14 12:19:21 +0100

    refactor: Also move error definition recalculation logic into least squares base class

--- minuit2.net/CostFunctions/LeastSquaresBase.cs
+++ minuit2.net/CostFunctions/LeastSquaresBase.cs
@@ -1,13 +1,14 @@
 namespace minuit2.net.CostFunctions;
 
-internal abstract class LeastSquaresBase : ICostFunction
+internal class LeastSquaresBase : ICostFunction
 {
-    protected readonly IReadOnlyList<double> X;
-    protected readonly IReadOnlyList<double> Y;
-    protected readonly Func<int, double> YErrorForIndex;
-    protected readonly Func<double, IReadOnlyList<double>, double> Model;
-    protected readonly Func<double, IReadOnlyList<double>, IReadOnlyList<double>>? ModelGradient;
-    protected readonly double ErrorDefinitionInSigma;
+    private readonly IReadOnlyList<double> _x;
+    private readonly IReadOnlyList<double> _y;
+    private readonly Func<int, double> _yErrorForIndex;
+    private readonly Func<double, IReadOnlyList<double>, double> _model;
+    private readonly Func<double, IReadOnlyList<double>, IReadOnlyList<double>>? _modelGradient;
+    private readonly double _errorDefinitionInSigma;
+    private readonly bool _isErrorDefinitionRecalculationEnabled;
 
     protected LeastSquaresBase(
         IReadOnlyList<double> x,
@@ -17,21 +18,23 @@ internal abstract class LeastSquaresBase : ICostFunction
         Func<double, IReadOnlyList<double>, double> model,
         Func<double, IReadOnlyList<double>, IReadOnlyList<double>>? modelGradient,
         double errorDefinitionInSigma,
-        double errorDefinitionScaling = 1)
+        double errorDefinitionScaling = 1,
+        bool isErrorDefinitionRecalculationEnabled = false)
     {
         if (x.Count != y.Count)
             throw new ArgumentException($"{nameof(x)} and {nameof(y)} must have the same length");
-        
-        X = x;
-        Y = y;
-        YErrorForIndex = yErrorForIndex;
-        Model = model;
-        ModelGradient = modelGradient;
-        ErrorDefinitionInSigma = errorDefinitionInSigma;
+
+        _x = x;
+        _y = y;
+        _yErrorForIndex = yErrorForIndex;
+        _model = model;
+        _modelGradient = modelGradient;
+        _errorDefinitionInSigma = errorDefinitionInSigma;
+        _isErrorDefinitionRecalculationEnabled = isErrorDefinitionRecalculationEnabled;
 
         Parameters = parameters;
         HasGradient = modelGradient != null;
-        
+
         // For least squares fits, an error definition of 1 corresponds to 1-sigma parameter errors
         // (4 would correspond to 2-sigma errors, 9 would correspond to 3-sigma errors etc.)
         ErrorDefinition = errorDefinitionInSigma * errorDefinitionInSigma * errorDefinitionScaling;
@@ -44,7 +47,7 @@ internal abstract class LeastSquaresBase : ICostFunction
     public double ValueFor(IReadOnlyList<double> parameterValues)
     {
         double sum = 0;
-        for (var i = 0; i < X.Count; i++)
+        for (var i = 0; i < _x.Count; i++)
         {
             var residual = ResidualFor(parameterValues, i);
             sum += residual * residual;
@@ -52,23 +55,53 @@ internal abstract class LeastSquaresBase : ICostFunction
 
         return sum;
     }
-    
+
     public IReadOnlyList<double> GradientFor(IReadOnlyList<double> parameterValues)
     {
         var gradientSums = new double[Parameters.Count];
-        for (var i = 0; i < X.Count; i++)
+        for (var i = 0; i < _x.Count; i++)
         {
             var residual = ResidualFor(parameterValues, i);
-            var gradients = ModelGradient!(X[i], parameterValues);
-            for (var j = 0; j < Parameters.Count; j++) 
-                gradientSums[j] -= 2 * residual * gradients[j] / YErrorForIndex(i);
+            var gradients = _modelGradient!(_x[i], parameterValues);
+            for (var j = 0; j < Parameters.Count; j++)
+                gradientSums[j] -= 2 * residual * gradients[j] / _yErrorForIndex(i);
         }
 
         return gradientSums;
     }
-    
+
     private double ResidualFor(IReadOnlyList<double> parameterValues, int index) =>
-        (Y[index] - Model(X[index], parameterValues)) / YErrorForIndex(index);
-    
-    public virtual ICostFunction WithErrorDefinitionRecalculatedBasedOnValid(IMinimizationResult result) => this;
+        (_y[index] - _model(_x[index], parameterValues)) / _yErrorForIndex(index);
+
+    public ICostFunction WithErrorDefinitionRecalculatedBasedOnValid(IMinimizationResult result)
+    {
+        if (!_isErrorDefinitionRecalculationEnabled) return this;
+
+        // Auto-scale the error definition such that a re-evaluation -- e.g., by a following minimization or accurate
+        // covariance computation (Hesse algorithm) -- yields the same parameter covariances that would be obtained
+        // from a minimization using an "ideal y-error" resulting in a reduced chi-squared value of 1.
+
+        // By scaling the error definition, the cost value (chi-squared value) itself won't be affected. This means
+        // that the resulting reduced chi-squared should approximate the variance of the noise overlying the data.
+
+        // This is equivalent to the default behavior in lmfit:
+        // https://lmfit.github.io/lmfit-py/fitting.html#uncertainties-in-variable-parameters-and-their-correlations
+
+        var resultParameters = result.Parameters.ToList();
+        var parameterValues = Parameters.Select(p => result.ParameterValues[resultParameters.IndexOf(p)]).ToArray();
+        var numberOfVariables = Parameters.Count(p => result.Variables.Contains(p));
+
+        var degreesOfFreedom = _x.Count - numberOfVariables;
+        var reducedChiSquared = ValueFor(parameterValues) / degreesOfFreedom;
+        return new LeastSquaresBase(
+            _x,
+            _y,
+            _yErrorForIndex,
+            Parameters,
+            _model,
+            _modelGradient,
+            _errorDefinitionInSigma, 
+            reducedChiSquared,
+            _isErrorDefinitionRecalculationEnabled);
+    }
 }
--- minuit2.net/CostFunctions/LeastSquaresWithUnknownYError.cs
+++ minuit2.net/CostFunctions/LeastSquaresWithUnknownYError.cs
@@ -8,33 +8,4 @@ internal class LeastSquaresWithUnknownYError(
     Func<double, IReadOnlyList<double>, IReadOnlyList<double>>? modelGradient,
     double errorDefinitionInSigma,
     double errorDefinitionScaling = 1)
-    : LeastSquaresBase(x, y, _ => 1, parameters, model, modelGradient, errorDefinitionInSigma, errorDefinitionScaling)
-{
-    public override ICostFunction WithErrorDefinitionRecalculatedBasedOnValid(IMinimizationResult result)
-    {
-        // Auto-scale the error definition such that a re-evaluation -- e.g., by a following minimization or accurate
-        // covariance computation (Hesse algorithm) -- yields the same parameter covariances that would be obtained
-        // from a minimization using an "ideal y-error" resulting in a reduced chi-squared value of 1.
-
-        // By scaling the error definition, the cost value (chi-squared value) itself won't be affected. This means
-        // that the resulting reduced chi-squared should approximate the variance of the noise overlying the data.
-
-        // This is equivalent to the default behavior in lmfit:
-        // https://lmfit.github.io/lmfit-py/fitting.html#uncertainties-in-variable-parameters-and-their-correlations
-
-        var resultParameters = result.Parameters.ToList();
-        var parameterValues = Parameters.Select(p => result.ParameterValues[resultParameters.IndexOf(p)]).ToArray();
-        var numberOfVariables = Parameters.Count(p => result.Variables.Contains(p));
-
-        var degreesOfFreedom = X.Count - numberOfVariables;
-        var reducedChiSquared = ValueFor(parameterValues) / degreesOfFreedom;
-        return new LeastSquaresWithUnknownYError(
-            X,
-            Y,
-            Parameters,
-            Model,
-            ModelGradient,
-            ErrorDefinitionInSigma,
-            reducedChiSquared);
-    }
-}
+    : LeastSquaresBase(x, y, _ => 1, parameters, model, modelGradient, errorDefinitionInSigma, errorDefinitionScaling, true);
