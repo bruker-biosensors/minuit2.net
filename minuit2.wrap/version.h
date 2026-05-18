e4de986bb80522335601e748a36e7b31740bd66c
Adrian Walser <adrian.walser@bruker.com>
2026-05-18 20:47:36 +0200

    ci: Add version metadata to native library files for Windows

---
+++ minuit2.wrap/version.h
@@ -0,0 +1,7 @@
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_BUILD 0
+#define VERSION_STRING "1.0.0.0"
+#define ORIGINAL_FILENAME "minuit2.native.dll"
+#define PRODUCT_NAME "Minuit2.NET"
